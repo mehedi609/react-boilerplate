b95ba681bd1efcf2f4f5ede29e5dfe12e4c84038
mehedi609 <mehedi609@gmail.com>
2019-07-27 00:12:30 +0600

    react-router-dom added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('root'));
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById("root"));
